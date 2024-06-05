f733aaabd18dce0496f60828678a9cd53c9de301
james-julius <jamesdarby7@gmail.com>
2024-06-04 22:42:33 -0700

    feat: add vs code config and fix router for detail page to be more robust, handling cases where some elements are not present

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration, RequestQueue } from 'crawlee';
+import { Log, PlaywrightCrawler, ProxyConfiguration, RequestQueue } from 'crawlee';
 import { Actor } from 'apify';
 import { router } from './routes.js';
 
@@ -7,16 +7,22 @@ import { router } from './routes.js';
 await Actor.init();
 'los-angeles-ca/2/'
 
-const baseUrl = 'https://www.apartments.com/los-angeles-ca'
+const baseUrl = 'https://www.apartments.com/los-angeles-ca/?bb=t_l92rt9uNml3q8u9E'
 
-const pages = new Array(17).fill('').map((_, idx) => `${baseUrl}/${idx + 1}`)
+const pages = new Array(17).fill('').map((_, idx) => `${baseUrl.split('?')[0]}?${idx + 1}${baseUrl.split('?')[1]}`)
 const startUrls = ['https://www.apartments.com/los-angeles-ca', ...pages];
+
+const logger = new Log({
+    level: 3
+});
 
 const crawler = new PlaywrightCrawler({
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
+    log: logger,
+    navigationTimeoutSecs: 2,
     // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+    // maxRequestsPerCrawl: 2,
 });
 
 await crawler.run(startUrls);
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,12 @@
-import { createPlaywrightRouter } from 'crawlee';
+import { createPlaywrightRouter, Configuration } from 'crawlee';
 
 export const router = createPlaywrightRouter();
+// Get the global configuration
+
+const config = Configuration.getGlobalConfig();
+// Set the 'persistStateIntervalMillis' option
+// of global configuration to 10 seconds
+config.set('persistStateIntervalMillis', 10_000);
 
 router.addDefaultHandler(async ({ enqueueLinks, log }) => {
     log.info("Reached new property listing page")
@@ -14,14 +20,19 @@ router.addDefaultHandler(async ({ enqueueLinks, log }) => {
 
 router.addHandler('detail', async ({ request, page, log, pushData }) => {
     const title = await page.title();
-    const propertyAddress = (await page.locator('#propertyAddressRow > *').allTextContents()).join(', ');
-    const propertyPhoneNumber = page.locator('.phoneNumber')
+    const propertyAddress = (await page.locator('#propertyAddressRow > *').allInnerTexts()).join(', ');
+    const propertyPhoneNumber = (await page.locator('.phoneNumber > a').getAttribute('href'))?.split(':')?.[1] || null
     const rentInfo = {
-        label: page.locator('.rentInfoLabel'),
-        detail: page.locator('.rentInfoDetail'),
+        label: await page.locator('p.rentInfoLabel').nth(0).innerText(),
+        detail: await page.locator('p.rentInfoDetail').nth(0).innerText(),
     }
     log.info(`${title}`, { url: request.loadedUrl });
-    const propertyWebsite = page.locator('a[title="View Property Website"]').textContent()
+    const hasPropertyWebsite = await page.isVisible('a[title="View Property Website"]')
+    let propertyWebsite = 'Not Provided'
+    if (hasPropertyWebsite) {
+        const propertyWebsiteLocator = await page.locator('a[title="View Property Website"]')
+        propertyWebsite = await propertyWebsiteLocator.getAttribute('href') || 'Not Provided'
+    }
     await pushData({
         url: request.loadedUrl,
         title,
