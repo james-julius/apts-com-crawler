744e1587b7adad57cc3ca7ed44ec84360d593f0f
james-julius <jamesdarby7@gmail.com>
2024-06-08 11:50:51 -0700

    feat: add apartment filter toggle and neighbourhood regex for context in csv file

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
 // For more information, see https://crawlee.dev/
-import { Log, PlaywrightCrawler, ProxyConfiguration, RequestQueue } from 'crawlee';
-import { firefox } from 'playwright';
+import { Log, PlaywrightCrawler, Dataset } from 'crawlee';
 import { Actor } from 'apify';
 import { router } from './routes.js';
 
 
 await Actor.init();
 
-const baseUrl = 'https://www.apartments.com/los-angeles-ca/'
-
-const pages = new Array(17).fill('').map((_, idx) => `${baseUrl}/${idx+1}`)
+// Rideshine service areas
+const baseUrls = [
+  "https://www.apartments.com/burbank-ca/",
+  "https://www.apartments.com/culver-city-ca/",
+  "https://www.apartments.com/el-segundo-ca/",
+  "https://www.apartments.com/glendale-ca/",
+  "https://www.apartments.com/hollywood-ca/",
+  "https://www.apartments.com/long-beach-ca/",
+  "https://www.apartments.com/los-angeles-ca/",
+  "https://www.apartments.com/marina-del-rey-ca/",
+  "https://www.apartments.com/north-hollywood-ca/",
+  "https://www.apartments.com/pasadena-ca/",
+  "https://www.apartments.com/san-pedro-ca/",
+  "https://www.apartments.com/santa-monica-ca/",
+  "https://www.apartments.com/sherman-oaks-ca/",
+  "https://www.apartments.com/south-pasadena-ca/",
+  "https://www.apartments.com/studio-city-ca/",
+  "https://www.apartments.com/toluca-lake-ca/",
+  "https://www.apartments.com/west-hollywood-ca/"
+];
+// const pages = new Array(17).fill('').map((_, idx) => `${baseUrl}/${idx+1}`)
 
 const logger = new Log({
     level: 4
@@ -21,9 +38,9 @@ const crawler = new PlaywrightCrawler({
     log: logger,
     requestHandlerTimeoutSecs: 300,
     // Comment this option to scrape the full website.
-    // maxRequestsPerCrawl: 2,
 });
 
-await crawler.run([baseUrl]);
+await crawler.run(baseUrls);
 
+await Dataset.exportToCSV('rideshine_service_area_apartments')
 await Actor.exit()
--- src/routes.ts
+++ src/routes.ts
@@ -1,45 +1,59 @@
-import { createPlaywrightRouter, Configuration, sleep } from 'crawlee';
+import { createPlaywrightRouter, sleep } from 'crawlee';
 import { getNextPageUrl } from './utils.js';
+import { error } from 'console';
 
 export const router = createPlaywrightRouter();
 // Get the global configuration
 
 
 
-router.addDefaultHandler(async ({ page, enqueueLinks, log }) => {
+router.addDefaultHandler(async ({ page, enqueueLinks, log, pushData }) => {
     let numPropertiesScraped = 0;
     const propertyDetailLinks: string[] = []
-    const hrefsScraped: { [key: string]: boolean } = {}
+
+    const neighbourHoodRegex = /(?<=https:\/\/www\.apartments\.com\/)(.*)(?=\/)/
+    const neighbourhood = neighbourHoodRegex.exec(page.url())?.[0] || 'Unknown';
+    log.info(`Enqueuing scraping for Neighbourhood: ${neighbourhood}`)
+
+    // Enable Apartments filter
+    try {
+
+        log.info("Enabling apartment filter")
+        await page.locator('#PropertyType-1').first().click()
+        log.info("Succeeded enabling apartment filter")
+    } catch (e) {
+        log.error(`Error occurred enabling apartment filter for neighbourhood: ${neighbourhood}.`, error)
+    }
 
     // Apartments.com runs each page navigation with JS. So we have to go from page to page within the same request
     // otherwise we see cryptic HTTP2 errors.
     while (true) {
         const currentUrl = page.url();
-        const propertyLinksSelector = 'header.placard-header div.property-information a.property-link'
+        const propertyLinksSelector = 'a.property-link'
         log.info(`Scraping listing page: ${currentUrl}`)
+        const propertiesToScrape: { [key: string]: boolean } = {}
 
         const propertyLinks = (await page.locator(propertyLinksSelector).all())
         for (const link of propertyLinks) {
-            numPropertiesScraped++
-            log.info(`Scraping property #${numPropertiesScraped}`)
-
             const linkHref = await link.getAttribute('href')
-
-            if (linkHref && linkHref in hrefsScraped) {
-                log.error(`Scraping duplicate href ${linkHref}`)
-
+            if (linkHref && linkHref in propertiesToScrape) {
+                log.info(`Duplicate href found: ${linkHref}`)
+                continue
             } else if (linkHref) {
-                hrefsScraped[linkHref] = true
+                propertiesToScrape[linkHref] = true
                 propertyDetailLinks.push(linkHref)
             }
         }
-
+        const numPropertiesToScrape = Object.keys(propertiesToScrape).length + 1;
+        numPropertiesScraped+= numPropertiesScraped
+        log.info(`Found ${numPropertiesToScrape} to scrape`)
         if (await page.isVisible('a.next')) {
             const nextPageUrl = getNextPageUrl(currentUrl);
             log.info(`Navigating to next page: ${nextPageUrl}`)
-
+            log.info("Clicking next page")
             await page.locator('a.next').click()
-            await sleep(5000)
+            log.info("Sleeping for 5 seconds")
+            await sleep(2000)
         } else {
             break;
         }
@@ -47,15 +61,21 @@ router.addDefaultHandler(async ({ page, enqueueLinks, log }) => {
     // Enqueue every property detail page and pass them into the detail request handler below
     await enqueueLinks({
         label: 'detail',
-        urls: propertyDetailLinks
+        urls: propertyDetailLinks,
+        userData: {
+            neighbourhood
+        }
     })
 });
 
 
-router.addHandler('detail', async ({ request, page, log, pushData }) => {
+router.addHandler('detail', async ({ request, page, log, userData, pushData }) => {
     const title = await page.title();
     const propertyAddress = (await page.locator('#propertyAddressRow > *').allInnerTexts()).join(', ');
-    const propertyPhoneNumber = (await page.locator('.phoneNumber > a').getAttribute('href'))?.split(':')?.[1] || null
+    let propertyPhoneNumber = null;
+    if (await page.locator('.phoneNumber > a').isVisible()) {
+        propertyPhoneNumber = (await page.locator('.phoneNumber > a').getAttribute('href'))?.split(':')?.[1] || null
+    }
     const rentInfo = {
         label: await page.locator('p.rentInfoLabel').nth(0).innerText(),
         detail: await page.locator('p.rentInfoDetail').nth(0).innerText(),
@@ -71,6 +91,8 @@ router.addHandler('detail', async ({ request, page, log, pushData }) => {
         url: request.loadedUrl,
         title,
         rentInfo,
+        // @ts-expect-error TODO: Type UserData
+        neighbourhood: userData.neighbourhood,
         propertyAddress,
         propertyPhoneNumber,
         propertyWebsite
